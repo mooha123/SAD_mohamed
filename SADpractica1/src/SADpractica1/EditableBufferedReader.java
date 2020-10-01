565e75e12077ccdfee41f9614ffc054527b1583f
mooha123 <mohamedguennoun12@gmail.com>
2020-10-01 19:39:07 +0200

    primera subida

---
+++ SADpractica1/src/SADpractica1/EditableBufferedReader.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SADpractica1;
+
+import java.io.BufferedReader;
+/**
+ *
+ * @author dat
+ */
+public class EditableBufferedReader {
+    //Atributos
+    
+    
+    //constructor
+    public EditableBufferedReader(){
+    
+    }
+    
+    //Funcions
+    public void setRaw(){
+    //Primerp debo utilizar el comando  'stty -echo raw' en la terminal
+    
+    //confirmar que se ha podido pasar!
+    
+    }
+}
