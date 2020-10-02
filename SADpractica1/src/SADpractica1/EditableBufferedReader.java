a10bde318b4d2df09ed08dc0a3832ff6ad18a4bd
mooha123 <mohamedguennoun12@gmail.com>
2020-10-02 11:27:47 +0200

    mi primer commit

--- SADpractica1/src/SADpractica1/EditableBufferedReader.java
+++ SADpractica1/src/SADpractica1/EditableBufferedReader.java
@@ -21,9 +21,37 @@ public class EditableBufferedReader {
     
     //Funcions
     public void setRaw(){
-    //Primerp debo utilizar el comando  'stty -echo raw' en la terminal
-    
-    //confirmar que se ha podido pasar!
-    
+        //Primero debo utilizar el comando  'stty -echo raw' en la terminal
+        //para que se ejecute en un terminal utilizo bin/sh
+        //Read commands from the command_string operand
+        //     instead of from the standard input.  Special
+        //     parameter 0 will be set from the command_name op‐
+        //     erand and the positional parameters ($1, $2, etc.)
+        //     set from the remaining argument operands.
+
+        String[] cadena = {"/bin/sh", "-c", "stty -echo raw </dev/tty"};
+        //confirmar que se ha podido pasar!
+        try{
+            Runtime.getRuntime().exec(cadena).waitFor();
+        }catch(Exception e){
+                System.out.println("Not put Terminal in raw mode");
+        }
+            
     }
+    
+    public void unsetRaw(){
+        //passa la consola de mode raw a mode cooked.
+        //lo mismo que el setraw
+        String[] cadena = {"/bin/sh", "-c", "stty -echo raw </dev/tty"};
+        //confirmar que se ha podido pasar!
+        try{
+            Runtime.getRuntime().exec(cadena).waitFor();
+        }catch(Exception e){
+            System.out.println("Not put Terminal in cooked mode");
+        }
+    }
+    
+    public void read(){
+    
+    } 
 }
