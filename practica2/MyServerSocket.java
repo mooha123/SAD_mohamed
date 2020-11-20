adfc1580e47570aa11ed7508a91b8b9bfdfdd7ae
mooha123 <56877178+mooha123@users.noreply.github.com>
2020-11-20 19:04:50 +0100

    Practica2 Acabada
    Falta revisar unas cuantas cosas pero en general esta todo hecho

--- practica2/MyServerSocket.java
+++ practica2/MyServerSocket.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package practica2;
 
 import java.io.*;
@@ -10,6 +5,7 @@ import java.net.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  *
@@ -17,20 +13,92 @@ import java.util.*;
  */
 public class MyServerSocket {
     private final ServerSocket serversocket;
+    private Map <String, Socket> pendingConnections = new ConcurrentHashMap<>();
+
 
     public MyServerSocket(int port) throws IOException{
         serversocket = new ServerSocket(port);
     }
     
-    public Socket listen() throws IOException {
-        Socket s = null;
+    public void listen() throws IOException {
+        Socket socket = null;
         try {
-            s = serversocket.accept();
-//            new Thread(new algo(s)).start();  //supongo que va el server
+            socket = serversocket.accept();
+            new Thread(new ControlThread(socket)).start();
+
         } catch (IOException ex) {
             Logger.getLogger(MyServerSocket.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return s;
+    }
+    
+    private class ControlThread implements Runnable {
+        private Socket clientSocket;
+        private PrintWriter out;
+        private BufferedReader in;
+        
+        public ControlThread (Socket socket) throws IOException {
+            clientSocket = socket;
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        }
+
+        public void run(){
+            try {
+                String connectionNick = in.readLine();
+                String remoteNick = in.readLine();
+
+                System.out.println("Connection: " + connectionNick + " " + remoteNick);
+                //Comprobamos que el nick no este en uso
+                if (!pendingConnections.containsKey(remoteNick + " " + connectionNick)){
+                    //Await in queue
+                    pendingConnections.put(connectionNick + " " + remoteNick, clientSocket);
+
+                }else {
+                    //Start connection
+                    Socket remoteSocket = pendingConnections.remove(remoteNick + " " +connectionNick);
+                    System.out.println(clientSocket);
+                    System.out.println(remoteSocket);
+                    new Developer(clientSocket, remoteSocket).start();
+                    new Developer(remoteSocket, clientSocket).start();
+                }
+  
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        private class Developer extends Thread {
+            private Socket rebut; 
+            private Socket enviat;
+
+            public Developer(Socket entry, Socket exit) {
+                this.rebut = entry;
+                this.enviat = exit;
+            }
+            
+            public void run(){
+                try {
+                    BufferedReader in = new BufferedReader(new InputStreamReader(rebut.getInputStream()));
+                    PrintWriter out = new PrintWriter(enviat.getOutputStream(), true);
+
+                    out.println("Oki");
+                    System.out.println(in);
+                    System.out.println(out);
+
+                    String inputLine;
+                    while ((inputLine = in.readLine()) != null) {
+                        out.println(inputLine);
+                        System.out.println(inputLine);
+                    }
+                    in.close();
+                    out.close();
+                    rebut.close();
+                    
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
     }
     
 }
--- practica2/Server.java
+++ practica2/Server.java
@@ -11,6 +11,7 @@ import java.io.IOException;
  * @author moham
  */
 public class Server {
+    
     public static void main(String args[]) throws IOException {
         MyServerSocket serverSocket = new MyServerSocket(6666);
         serverSocket.listen();
