5ee436575444a15bc286e08486f42036e0a229e7
mooha123 <56877178+mooha123@users.noreply.github.com>
2020-11-28 23:45:58 +0100

    falta hacer el controller model y acabar el view

---
+++ snake/model_snake.py
@@ -0,0 +1,26 @@
+import pygame 
+import view_snake
+
+WIN_WIDTH = 600
+WIN_HEIGHT = 400
+RED = (255, 0, 0)
+GREEN1 = (191, 255, 0)
+GREEN2 = (0, 255, 0)
+PURPLE =  (204, 153, 255)
+ObjectSize = 30
+
+all_sprites = pygame.sprite.Group()
+
+class Block(pygame.sprite.Sprite):
+
+class Snake(object): 
+
+
+class main():
+    def __init__(self):
+        all_sprites.empty()
+
+        while True:...    
+
+        
+            
---
+++ snake/snake_game.py
@@ -0,0 +1,7 @@
+import pygame 
+import random 
+import view_snake
+
+def main():
+    view_snake.main()
+main()
