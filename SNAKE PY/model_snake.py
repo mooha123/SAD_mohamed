67db0c9d4b08d6179f3d15b87d47207f77e7b874
mooha123 <56877178+mooha123@users.noreply.github.com>
2020-11-30 18:13:26 +0100

    Snake python acabat
    projecte de snake en python acabat, amb diferents nivells i amb la biblioteca  pygame

---
+++ SNAKE PY/model_snake.py
@@ -0,0 +1,237 @@
+import pygame 
+import random 
+import controller_snake
+import view_snake
+
+WIN_WIDTH = 600
+WIN_HEIGHT = 400
+DELAY = 0.15
+RED = (255, 0, 0)
+YELLOW = (191, 255, 0)
+GREEN2 = (0, 155, 0)
+WHITE =  (255, 255, 255)
+BLUE =  (22, 229, 201 )
+ObjectSize = 20
+all_sprites = pygame.sprite.Group()
+obstacles = pygame.sprite.Group()
+
+
+#set_timer(TICK, T) T= 100ms
+# direccio en vector x,y normal (1,0) i sumar el event que tenim (si es 0 la suma vol dir que es direcc contraria)
+
+#subclase de sprite
+class Block(pygame.sprite.Sprite):
+    def __init__(self, start, head=False, color= GREEN2 ):
+        super().__init__()
+        
+        self.head = head
+        self.image = pygame.Surface([ObjectSize, ObjectSize])
+        self.image.fill(color)
+
+        if self.head:
+            self.image1 = pygame.image.load("SnakeHeadGreen.png")
+            self.image = self.image1
+
+        self.rect = self.image.get_rect()
+        # es posa a la posició que li pasem
+        self.rect.x = start[0]
+        self.rect.y = start[1]
+
+    def rotate(self, direction, changeDir):
+        if direction == [1, 0] and changeDir == True:
+            self.image = pygame.transform.rotate(self.image1, 270)
+        if direction == [-1, 0] and changeDir == True:
+            self.image = pygame.transform.rotate(self.image1, 90)
+        if direction == [0, 1] and changeDir == True:
+            self.image = pygame.transform.rotate(self.image1, 0)
+        if direction == [0, -1] and changeDir == True:
+            self.image = pygame.transform.rotate(self.image1, 180)
+
+class Apple(pygame.sprite.Sprite):
+    def __init__(self, color=RED):
+        super().__init__()
+        apple = pygame.image.load("Apple.png")
+        self.image = apple
+        self.rect = self.image.get_rect()
+    def randomPos(self, group):
+        #crea una poma aleatoria i després mira que no estigui sobre el cos de la serp i si ho està torna a crear una altre.
+        while True:
+            self.rect.x = random.randrange(ObjectSize, WIN_WIDTH, ObjectSize)
+            self.rect.y = random.randrange(ObjectSize, WIN_HEIGHT, ObjectSize)
+            collide = pygame.sprite.spritecollideany(self, group)
+            if not collide:
+                break
+
+class Snake(object):
+    body_group = pygame.sprite.Group()
+    body = []
+    def __init__(self, pos, image):
+        
+        self.head = Block(pos, True, BLUE)
+        #self.head = image
+        self.body_group.add(self.head)
+        all_sprites.add(self.head)
+        self.direction = [1, 0]
+        self.head.rotate(self.direction, True)
+        self.eaten = False 
+        self.key = controller_snake.Detection()
+        self.ok = True
+
+    
+
+    # Aquesta es la funcio principal que fa que a cada tic de rellotge la serp es mogui i es on apliquem
+    # la deteccio de colisions, l'augment del tamany de la serp y es llegeix la tecla que es prem per 
+    # fer els canvis de direccio que pertoquin
+    def sum(self, vect):
+        aux = [self.direction[0]+vect[0], self.direction[1]+vect[1]]
+        if aux == [0, 0] or vect ==[0, 0] or self.direction == vect:
+            return False
+        else: 
+            return True
+    
+    def move(self, apple, normal):
+        detect = self.key.keyDetection()
+        accept = self.sum(detect) #retorna true o fals depenent del moviment
+        #print(accept)
+        if accept:
+            self.direction = detect
+        self.bodySize = len(self.body)
+        
+        #Si eaten es Fals vol dir que ens movem sense afegir segment per tant posem el ultim segment
+        #a la posicio del cap i reordenem el conjunt
+        if not self.eaten and self.bodySize>0:   
+
+            self.body[-1].rect.x = self.head.rect.x
+            self.body[-1].rect.y = self.head.rect.y
+            self.body.insert(0, self.body.pop())
+            
+        elif self.eaten:
+            
+            self.body_group.add(self.head)
+            self.newSegment()
+            apple.randomPos(self.body_group)
+
+        # comprueba la dirección, y en caso de superar los limites de la ventana aparece por el otro lado
+        if normal:
+            if self.direction == [1, 0]:
+                self.head.rect.x += ObjectSize
+                if self.head.rect.x >= WIN_WIDTH:
+                    self.head.rect.x = 0
+            elif self.direction == [-1, 0]:
+                self.head.rect.x -= ObjectSize
+                if self.head.rect.x < 0:
+                    self.head.rect.x = WIN_WIDTH - ObjectSize
+            elif self.direction == [0, 1]:
+                self.head.rect.y -= ObjectSize
+                if self.head.rect.y < 0:
+                    self.head.rect.y = WIN_HEIGHT - ObjectSize
+            elif self.direction == [0, -1]:
+                self.head.rect.y += ObjectSize
+                if self.head.rect.y >= WIN_HEIGHT:
+                    self.head.rect.y = 0
+        else:
+            if self.direction == [1, 0]:
+                self.head.rect.x += ObjectSize
+            elif self.direction == [-1, 0]:
+                self.head.rect.x -= ObjectSize
+            elif self.direction == [0, 1]:
+                self.head.rect.y -= ObjectSize
+            elif self.direction == [0, -1]:
+                self.head.rect.y += ObjectSize
+
+        self.head.rotate(self.direction, accept)
+
+        self.eaten = False
+
+        if self.body_group.has(self.head):
+            self.body_group.remove(self.head)
+
+        self.collisionDetect(normal)
+        self.eaten = self.eat(apple)
+
+    #Fa les comparacions de la posicio del cap de la serp per saber si esta colisionant amb qualsevol cosa
+    def collisionDetect(self, normal):
+
+        col = pygame.sprite.spritecollideany(self.head, self.body_group)
+        if col: 
+            self.ok = False
+        elif pygame.sprite.spritecollideany(self.head, obstacles):
+            self.ok = False
+        elif not normal:
+            if self.head.rect.x < 0 or self.head.rect.x > (WIN_WIDTH - ObjectSize + 10):
+                self.ok = False
+            elif self.head.rect.y < 0 or self.head.rect.y > (WIN_HEIGHT - ObjectSize + 10):
+                self.ok = False
+
+    # Funcio que ens permet saber si el cap esta en la mateixa posicio que la poma i per tant 
+    # se la ha menjat
+    def eat(self, apple):
+        sound = pygame.mixer.Sound("Beep.mp3")
+        if self.head.rect.x == apple.rect.x and self.head.rect.y == apple.rect.y:
+            sound.play()
+            return True 
+        else: 
+            return False
+
+    # Crea un nou segment i el posa a la posicio antiga del cap abans de que aquest es mogui
+    def newSegment(self):
+
+        pos = [self.head.rect.x, self.head.rect.y]
+        newSegment = Block(pos)
+        self.body.insert(0, newSegment)
+        self.body_group.add(newSegment) #  el segment als sprites del cos de la serp.
+        all_sprites.add(newSegment) # Afegim al grup de tots els sprites.
+        self.bodySize = len(self.body) # modifiquem la llargada del cos
+
+class Obstacles(object): #barrier
+    def __init__(self):
+        self.obsatacles_arr = []
+
+    def pared(self, group):
+
+        ran = 20 # range
+        pos_x = (WIN_WIDTH/2) - (ran/2 * 20) #WIN_WIDTH/2 - (range/2 * 20)
+        pos_y = (WIN_HEIGHT/2)
+        pos = [pos_x, pos_y]
+        for x in range(20):
+            #afegim barrier a sprite
+            barrier = Block(pos, False, YELLOW)
+            pos_x += ObjectSize
+            self.obsatacles_arr.append(barrier)
+            obstacles.add(barrier)
+            all_sprites.add(barrier)
+            pos = [pos_x, pos_y]
+
+class main():
+    def __init__(self, velocitat, normal):
+        all_sprites.empty()
+        obstacles.empty()
+        obs = Obstacles()
+        window = view_snake.Window(WIN_WIDTH, WIN_HEIGHT)
+        head = pygame.image.load("SnakeHeadGreen.png")
+
+        clock = pygame.time.Clock()
+        start = [100, 100]
+        s = Snake(start, head)
+        apple = Apple(RED)
+        all_sprites.add(apple)
+        apple.randomPos(s.body_group)
+        sound2 = pygame.mixer.Sound("GameOver2.mp3")
+        
+        while True:     
+            s.move(apple, normal)
+
+            if not s.ok:
+                break
+            score = "Score = " + str(len(s.body))
+            if not normal:
+                obs.pared(all_sprites)
+                velocitat += 0.05
+
+            window.update(all_sprites, score)
+            clock.tick(velocitat)
+
+        sound2.play()
+        print("END GAME")
+        
+            
